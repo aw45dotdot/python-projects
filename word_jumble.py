b421eff1edbc1f7a81a67ea6489bce8f0897597a
aw45dotdot <muhammadbaig2912@gmail.com>
2025-06-09 18:01:14 +0100

    Update word_jumble.py

--- word_jumble.py
+++ word_jumble.py
@@ -1,6 +1,6 @@
 import random
 
-word_list = ["java","anaconda","ruby","python","html","javascript","hermes"]
+word_list = ["java", "anaconda", "ruby", "python", "html", "javascript", "hermes", "dictionary", "word", "jumble", "treasure", "hunt"]
 
 def jumble_word(word):
     word_list = list(word)
@@ -17,3 +17,30 @@ def play_game():
     for round_number in range(1, rounds + 1):
         word = random.choice(word_list)
         scrambled_word = jumble_word(word)
+
+        print("YOUR TASK IS TO GUESS THE ORIGINAL WORD: ")
+
+        print(f'\nCURRENT ROUND:{round_number}')
+
+        print(f'SCRAMBLED WORD IS: {scrambled_word}')
+
+        hint_choice = input("DO YOU WANT A HINT(Y/N): ").lower()
+        if hint_choice == "y":
+            print(get_hint(word))
+        
+        guess = input("GUESS ORIGNAL WORD: ").lower()
+        while not guess.isalpha():
+            print("ENTER VALID WORD: ")
+            guess = input("GUESS ORIGNAL WORD: ").lower()
+
+        if guess == word:
+                print("GOOD JOB")
+                score += 1
+        else:
+                print("BAD LUCK")
+                print(f'CORRECT WORD WAS: {word}')
+
+    print(f'GAME OVER, YOUR FINAL SCORE IS: {score}/{rounds}')
+
+#calling functions
+play_game()
